bacacad915a95d7215665af14ce12aae40387c48
Anilkumar0017 <eete.kumar@iitgn.ac.in>
2025-11-08 22:03:06 +0530

    Added clean Lab 10 assignment

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int i = int.MaxValue;
+            Console.WriteLine(-(i + 1) - i);
+            for (i = 0; i <= int.MaxValue; i++); // note the semicolon!
+            Console.WriteLine("Program ended!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
